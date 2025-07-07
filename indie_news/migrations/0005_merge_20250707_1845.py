37ad3e49e9516e6558bc9f9d736ca10c00dd63e8
Keyth Kazumi <kazumimika@gmail.com>
2025-07-07 18:29:38 -0300

    Merges back with code changes

---
+++ indie_news/migrations/0005_merge_20250707_1845.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('indie_news', '0004_indienewstask_output_and_more'),
+    ]
+
+    operations = [
+    ]
