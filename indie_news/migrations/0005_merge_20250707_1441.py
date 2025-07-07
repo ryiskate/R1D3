bf995619892f2651647f340602ca5d399893596a
Keyth Kazumi <kazumimika@gmail.com>
2025-07-07 14:50:39 -0300

    Fix migration dependency conflicts between indie_news and projects apps

---
+++ indie_news/migrations/0005_merge_20250707_1441.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('indie_news', '0004_indienewstask_output_and_more'),
+        ('projects', '0015_team_arcadetask_team_educationtask_team_and_more'),
+    ]
+
+    operations = [
+    ]
